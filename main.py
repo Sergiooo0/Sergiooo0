2774ec4e00a0f7780426f123ed8976e11b4dfb6a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-18 22:15:42 +0100

    Added Markdown table with repositories

--- main.py
+++ main.py
@@ -13,10 +13,31 @@ markDown = f"""## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/gip
 
 
 if __name__ == "__main__":
-    print(githubInfo.getRepositoriesWithCommits())
+    repositories = githubInfo.getRepositoriesWithCommits()
+
+    # We order the repositories by the number of commits in descending order
+    # And in case of a tie, we order them alphabetically
+    repositories = dict(
+        sorted(
+            repositories.items(), key=lambda item: (-item[1], item[0]), reverse=False
+        )
+    )
 
     print(f"Number of API calls: {githubInfo.NUMBERCALLSAPI}")
     print(f"Number of normal calls: {githubInfo.NUMBERCALLSNORMAL}")
+
+    # Now we make a markdown table with the repositories and the commit count
+    markDownTable = """
+| <img width="1000"><br>Repository<br><img width="1000" height="1"> | <img width="1000" height="1"><br>Commits<br><img width="1000" height="1">  |
+|:----------|----------:|
+"""
+
+    for repository, commitCount in repositories.items():
+        repoName = repository[len(githubInfo.BASE) :]
+        markDownTable += f"| [{repoName}]({repository}) | {commitCount} |\n"
+
+    # We add the table
+    markDown += markDownTable
 
     # Now we will write the markdown to README.md
     with open("README.md", "w") as file:
