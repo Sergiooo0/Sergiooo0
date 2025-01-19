598b6683851e8f31c18a2302d9f92d805815d49e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-01-19 11:11:18 +0100

    Formatted html

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import githubInfo
 markDown = f"""## <img src="https://media.giphy.com/media/iY8CRBdQXODJSCERIr/giphy.gif" width="25"><b> Github Stats </b>
 
 <p align="center">
-<a href="https://github.com/{githubInfo.AUTHOR}">
-  <img width="600px" src="https://github-readme-stats-liard-nu-21.vercel.app/api?username={githubInfo.AUTHOR}&show_icons=true&hide_title=true&show=reviews,prs_merged&include_all_commits=true" alt="GitHub Stats"/>
-  <img width="600px" src="https://github-readme-stats-liard-nu-21.vercel.app/api/top-langs/?username={githubInfo.AUTHOR}&show_icons=true"/>
-  
-</a>
+  <a href="https://github.com/{githubInfo.AUTHOR}">
+    <img
+      width="600px"
+      src="https://github-readme-stats-liard-nu-21.vercel.app/api?username={githubInfo.AUTHOR}&show_icons=true&hide_title=true&show=reviews,prs_merged&include_all_commits=true"
+      alt="GitHub Stats"
+      />
+    <img
+      width="600px"
+      src="https://github-readme-stats-liard-nu-21.vercel.app/api/top-langs/?username={githubInfo.AUTHOR}&show_icons=true"
+      />
+  </a>
 </p>
 """
 
