203643a3613f375649fa6d7009254e648959fca2
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-05-21 15:28:02 +0200

    Eliminate comment

--- main.py
+++ main.py
@@ -68,7 +68,6 @@ if __name__ == "__main__":
     # We add the table
     markDown += markDownTable
 
-    # # Now we will write the markdown to README.md
-    # with open("README.md", "w") as file:
-    #     file.write(markDown)
-
+    # Now we will write the markdown to README.md
+    with open("README.md", "w") as file:
+        file.write(markDown)
